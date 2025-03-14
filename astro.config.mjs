e122c2ce084719d666fb459da5c79c548b0f6696
RJL <rleyva@tutanota.com>
2025-03-14 17:52:37 -0400

    import mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,16 @@ import { defineConfig } from 'astro/config'
 
 import react from '@astrojs/react'
 
+import mdx from '@astrojs/mdx'
+
 // https://astro.build/config
 export default defineConfig({
+  integrations: [react(), mdx()],
   devToolbar: {
     enable: true
   },
-
   server: {
     port: 3000
-  },
-
-  integrations: [react()]
+  }
 })
+
