385d46de9b9bf35cb7a7a42cf327ee9bf8516a51
RJL <rleyva@tutanota.com>
2025-03-14 07:08:49 -0400

    add prettier personal preference

---
+++ .prettierrc.mjs
@@ -0,0 +1,19 @@
+/** @type {import("prettier").Config} */
+export default {
+  arrowParens: "avoid",
+  singleQuote: true,
+  bracketSpacing: true,
+  endOfLine: "lf",
+  semi: false,
+  tabWidth: 2,
+  trailingComma: "none",
+  plugins: ["prettier-plugin-astro"],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+};
