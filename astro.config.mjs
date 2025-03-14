13f7f02db099f18b53a53ca0c9ef755560f6e4a0
RJL <rleyva@tutanota.com>
2025-03-14 12:09:00 -0400

    import react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 import { defineConfig } from 'astro/config'
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
   devToolbar: {
     enable: true
   },
+
   server: {
     port: 3000
-  }
+  },
+
+  integrations: [react()]
 })
