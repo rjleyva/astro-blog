3d93557881147e7f0c79f9db9f804390b41cf085
RJL <rleyva@tutanota.com>
2025-03-15 08:26:59 -0400

    remove icons and change it to labels

--- src/components/react/Socials/Socials.tsx
+++ src/components/react/Socials/Socials.tsx
@@ -1,76 +1,42 @@
 import type { JSX } from 'astro/jsx-runtime'
-import GithubIcon from '../icons/GithubIcon'
-import LinkedinIcon from '../icons/LinkedinIcon'
-import InstagramIcon from '../icons/InstagramIcon'
 
 interface SocialProps {
   id: string
+  label: string
   url: string
   ariaLabel: string
   target: string
   rel: string
-  icon: JSX.Element
 }
 
 const Social: SocialProps[] = [
   {
     id: 'GitHub Link',
+    label: 'Github',
     url: 'https://github.com/rjleyva/',
     ariaLabel: 'GitHub Icon',
     target: '_blank',
-    rel: 'noopener noreferrer',
-    icon: (
-      <GithubIcon
-        width={24}
-        height={24}
-        color="currentColor"
-        ariaLabel="GitHub Icon"
-        role="img"
-        title="Connect with RJ Leyva on GitHub"
-      />
-    )
+    rel: 'noopener noreferrer'
   },
   {
     id: 'LinkedIn Link',
+    label: 'Linkedin',
     url: 'https://linkedin.com/rjleyva/',
     ariaLabel: 'LinkedIn Icon',
     target: '_blank',
-    rel: 'noopener noreferrer',
-    icon: (
-      <LinkedinIcon
-        width={24}
-        height={24}
-        color="currentColor"
-        ariaLabel="GitHub Icon"
-        role="img"
-        title="Connect with RJ Leyva on GitHub"
-      />
-    )
+    rel: 'noopener noreferrer'
   },
   {
     id: 'Instagram Link',
+    label: 'Instagram',
     url: 'https://instagram.com/slammedwigo/',
     ariaLabel: 'Instagram Icon',
     target: '_blank',
-    rel: 'noopener noreferrer',
-    icon: (
-      <InstagramIcon
-        width={24}
-        height={24}
-        color="currentColor"
-        ariaLabel="GitHub Icon"
-        role="img"
-        title="Connect with RJ Leyva on GitHub"
-      />
-    )
+    rel: 'noopener noreferrer'
   }
 ]
 
-interface SocialsProps {
-  cta: string
-}
-
-const Socials = ({ cta }: SocialsProps) => {
+const Socials = () => {
   return (
     <section>
       <div
@@ -80,19 +46,18 @@ const Socials = ({ cta }: SocialsProps) => {
           gap: '0.5rem'
         }}
       >
-        <p>{cta}</p>
         <ul
           style={{
             display: 'flex',
-            gap: '0.2rem',
+            gap: '0.7rem',
             listStyle: 'none',
             marginBlock: '1rem'
           }}
         >
-          {Social.map(({ id, url, ariaLabel, target, rel, icon }) => (
+          {Social.map(({ id, url, ariaLabel, target, rel, label }) => (
             <li key={id}>
               <a href={url} aria-label={ariaLabel} target={target} rel={rel}>
-                {icon}
+                {label}
               </a>
             </li>
           ))}
